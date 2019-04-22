2c5bc4fbb60bbe6c5e9ae1f8828b78a22cd046f9
Beril Febriyansyah <berildudun@gmail.com>
2019-04-22 17:49:29 +0700

    Web CodeIgniter
    By Beril Feb

---
+++ application/controllers/Coba.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Coba extends CI_Controller {
+	
+public function index()
+	{
+		$this->load->view('welcome');
+	}
+
+public function latihan()
+	{
+		$this->load->view('welcome');
+	}
+
+public function tambah()
+	{
+		$this->load->view('welcome');
+	}
+
+public function hapus()
+	{
+		$this->load->view('welcome');
+	}
+
+public function edit()
+	{
+		$this->load->view('welcome');
+	}
+
+}
